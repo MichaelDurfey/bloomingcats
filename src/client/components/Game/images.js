6d25ab6deabc2ce0719c68092efd5b03c43407c6
Michael Durfey <mdurfey@me.com>
2020-06-14 12:06:19 -0700

    chore(assets): update assets

--- src/client/components/Game/images.js
+++ src/client/components/Game/images.js
@@ -5,13 +5,13 @@ import orangeCat from '../../../assets/4OrangeCat.png';
 import greyCat from '../../../assets/5GreyCat.png';
 import calicoCat from '../../../assets/6CalicoCat.png';
 import siameseCat from '../../../assets/7SiameseCat.png';
-import brownCatSelected from '../../../assets/1BrownCatSelected2.gif';
-import whiteCatSelected from '../../../assets/2WhiteCatSelected2.gif';
-import blackCatSelected from '../../../assets/3BlackCatSelected2.gif';
-import orangeCatSelected from '../../../assets/4OrangeCatSelected2.gif';
-import greyCatSelected from '../../../assets/5GreyCatSelected2.gif';
-import calicoCatSelected from '../../../assets/6CalicoCatSelected2.gif';
-import siameseCatSelected from '../../../assets/7SiameseCatSelected2.gif';
+import brownCatSelected from '../../../assets/1BrownCatSelected1.gif';
+import whiteCatSelected from '../../../assets/2WhiteCatSelected.gif';
+import blackCatSelected from '../../../assets/3BlackCatSelected.gif';
+import orangeCatSelected from '../../../assets/4OrangeCatSelected.gif';
+import greyCatSelected from '../../../assets/5GreyCatSelected.gif';
+import calicoCatSelected from '../../../assets/6CalicoCatSelected.gif';
+import siameseCatSelected from '../../../assets/7SiameseCatSelected.gif';
 
 const catImageMap = [brownCat, whiteCat, blackCat, orangeCat, greyCat, calicoCat, siameseCat];
 const catImageSelectedMap = [brownCatSelected,
