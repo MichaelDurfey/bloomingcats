e33797c6ed6caf44b9b645308762cdc63f0e3349
Michael Durfey <mdurfey@me.com>
2020-06-16 22:33:17 -0700

    feat: touchscreen support

--- src/client/components/Game/index.jsx
+++ src/client/components/Game/index.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import { TouchBackend } from 'react-dnd-touch-backend';
 import Board from './Board';
 import BoardContextProvider from './BoardContext';
+
+function isTouchDevice() {
+  // eslint-disable-next-line no-undef
+  if (('ontouchstart' in window) || (window.DocumentTouch && document instanceof DocumentTouch)) {
+    return true;
+  }
+  return false;
+}
 
 export default function Game() {
   return (
     <section>
-      <DndProvider backend={HTML5Backend}>
+      <DndProvider backend={isTouchDevice() ? TouchBackend : HTML5Backend}>
         <BoardContextProvider>
           <Board />
         </BoardContextProvider>
