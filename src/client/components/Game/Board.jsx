4edb7b3a80b26a5dc4abaa478019370b97bf85ba
Michael Durfey <mdurfey@me.com>
2020-07-05 16:09:30 -0700

    feat: move music to header

--- src/client/components/Game/Board.jsx
+++ src/client/components/Game/Board.jsx
@@ -4,7 +4,6 @@ import styles from '../../styles/Board.css';
 import { useBoardContext } from './BoardContext';
 import Score from './Score';
 import Next from './Next';
-import Music from './Music';
 
 export default function Board() {
   const { board, boardContainer, nextAndScore } = styles;
@@ -14,7 +13,6 @@ export default function Board() {
       <div className={nextAndScore}>
         <Score />
         <Next />
-        <Music />
       </div>
       <div className={board}>
         { squares }
--- src/client/components/Header.jsx
+++ src/client/components/Header.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import Music from './Game/Music';
 
 export default function Header() {
   return (
     <>
       <Navbar bg="dark" variant="dark">
         <Navbar.Brand href="/">Kitten Pop</Navbar.Brand>
-        <Nav className="mr-auto">
+        <Nav>
           <Nav.Link href="/game">Game</Nav.Link>
           <Nav.Link href="/rules">Rules</Nav.Link>
+          <Nav.Link>
+            <Music />
+          </Nav.Link>
         </Nav>
       </Navbar>
     </>
