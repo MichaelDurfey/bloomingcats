d34cf5430b6d19d2a8aff8bfd0e1222810f4960d
Michael Durfey <mdurfey@me.com>
2020-07-07 21:33:20 -0700

    feat: copywrite

--- src/client/components/Footer.jsx
+++ src/client/components/Footer.jsx
@@ -4,7 +4,10 @@ import Navbar from 'react-bootstrap/Navbar';
 export default function Footer() {
   return (
     <>
-      <Navbar bg="dark" variant="dark">
+      <Navbar className="justify-content-between" bg="dark" variant="dark">
+        <Navbar.Text>
+          &copy; 2020 by Michael Durfey. All rights reserved. &nbsp;
+        </Navbar.Text>
         <Navbar.Text>
           Made with
           <span role="img" aria-label="heart"> ❤️ </span>
