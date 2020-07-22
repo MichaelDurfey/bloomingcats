59e98c5aa8be336ae1fd6278c64e5c982e5ec128
Michael Durfey <mdurfey@me.com>
2020-07-21 20:23:19 -0700

    feat: text and a button

--- src/client/components/Home/index.jsx
+++ src/client/components/Home/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 import styles from '../../styles/Home.css';
 import { randomCat } from '../Game/BoardContext';
 
@@ -15,6 +16,9 @@ export default function Home() {
               <h1>
                 Welcome to Kitten Pop!
               </h1>
+              <p>Kitten Pop is a fun game with cats. Try to score as many points as you can before the board is overrun with cats. Score points by arranging 5 or more cats in a row or diagonal in any direction. Watch out though, because there must be a clear path to the square to place a cat.</p>
+              <br />
+              <Button variant="danger" href="/game">Start!</Button>
               <img alt="" className={styles.image} src={randomCat().img} />
             </div>
           </Col>
